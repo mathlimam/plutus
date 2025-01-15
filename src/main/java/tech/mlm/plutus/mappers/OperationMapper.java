670bc574fe01f9689d1f783a702602f23d851a3d
Matheus <math.lima.m@gmail.com>
2025-01-15 16:24:40 -0300

    refact: refactoring toEntity method and adding toDto method

--- src/main/java/tech/mlm/plutus/mappers/OperationMapper.java
+++ src/main/java/tech/mlm/plutus/mappers/OperationMapper.java
@@ -3,40 +3,41 @@ package tech.mlm.plutus.mappers;
 import org.springframework.stereotype.Component;
 import tech.mlm.plutus.entities.OperationEntity;
 import tech.mlm.plutus.dtos.OperationDTO;
-import tech.mlm.plutus.services.ProductService;
-import tech.mlm.plutus.services.SellerService;
-import tech.mlm.plutus.services.StoreService;
-import tech.mlm.plutus.types.OperationType;
-import tech.mlm.plutus.types.StatusType;
+import tech.mlm.plutus.services.OperationService;
+
+
+import java.util.Optional;
 
 @Component
 public class OperationMapper {
-    private final StoreService storeService;
-    private final SellerService sellerService;
-    private final ProductService productService;
+    private final OperationService operationService;
 
-    public OperationMapper(StoreService storeService, SellerService sellerService, ProductService productService) {
-        this.storeService = storeService;
-        this.sellerService = sellerService;
-        this.productService = productService;
+    public OperationMapper(OperationService operationService) {
+        this.operationService = operationService;
     }
 
-    public OperationEntity toEntity(OperationDTO dto){
-        OperationEntity entity = new OperationEntity();
-        entity.setType(OperationType.valueOf(dto.operationType()));
-        entity.setCreatedAt(dto.createdAt());
-        entity.setUpdatedAt(dto.updatedAt());
-        entity.setStatus(StatusType.valueOf(dto.statusType()));
-        entity.setOriginSeller(sellerService.findByCpf(dto.originSellerId()).get());
-        entity.setDestinationSeller(sellerService.findByCpf(dto.destinationSellerId()).get());
-        entity.setProductEntity(productService.findByBarcode(dto.productId()).get());
-        entity.setQuantity(dto.quantity());
-        entity.setOriginStore(storeService.findById(dto.originStoreId()).get());
-        entity.setDestinationStore(storeService.findById(dto.destinationStoreId()).get());
+    public Optional<OperationEntity> toEntity(OperationDTO dto){
 
-        System.out.println(entity.toString());
-        return entity;
+        try {
+            if (dto == null) throw new IllegalArgumentException("dto cannot be null");
+            return Optional.of(operationService.createOperationEntity(dto));
 
+        } catch (Exception e){
+            System.out.println(e.getLocalizedMessage());
+            return Optional.empty();
+        }
+    }
 
+    public OperationDTO toDto(OperationEntity entity){
+        return new OperationDTO(entity.getType().name(),
+                entity.getCreatedAt(),
+                entity.getUpdatedAt(),
+                entity.getStatus().name(),
+                entity.getOriginSeller().getCpf(),
+                entity.getDestinationSeller().getCpf(),
+                entity.getOriginStore().getId(),
+                entity.getDestinationStore().getId(),
+                entity.getProductEntity().getBarcode(),
+                entity.getQuantity());
     }
 }
