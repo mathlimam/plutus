7223166c515c95f5dd83f5a4281b1d85e0e2e1bd
Matheus <math.lima.m@gmail.com>
2025-01-15 14:23:07 -0300

    add pom.xml and PlutusApplication.java

---
+++ src/main/java/tech/mlm/plutus/PlutusApplication.java
@@ -0,0 +1,17 @@
+package tech.mlm.plutus;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class PlutusApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PlutusApplication.class, args);
+
+	}
+
+}
