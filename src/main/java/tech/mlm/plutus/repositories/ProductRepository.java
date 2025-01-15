63d7876a4aa4be8cbc71e1f30a9ed2e08a76648c
Matheus <math.lima.m@gmail.com>
2025-01-15 14:22:38 -0300

    refact: refactoring repositories

--- src/main/java/tech/mlm/plutus/repositories/ProductRepository.java
+++ src/main/java/tech/mlm/plutus/repositories/ProductRepository.java
@@ -3,4 +3,8 @@ package tech.mlm.plutus.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tech.mlm.plutus.entities.ProductEntity;
 
-public interface ProductRepository extends JpaRepository<ProductEntity, Long> {}
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+    Optional<ProductEntity> findByBarcode(String barcode);
+}
--- src/main/java/tech/mlm/plutus/repositories/SellerRepository.java
+++ src/main/java/tech/mlm/plutus/repositories/SellerRepository.java
@@ -3,4 +3,8 @@ package tech.mlm.plutus.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tech.mlm.plutus.entities.SellerEntity;
 
-public interface SellerRepository extends JpaRepository<SellerEntity, Long> {}
+import java.util.Optional;
+
+public interface SellerRepository extends JpaRepository<SellerEntity, Long> {
+    Optional<SellerEntity> findByCpf(String cpf);
+}
