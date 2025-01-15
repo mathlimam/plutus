2348c9dead725804237f574adfce2fe20ef0f32b
Matheus <math.lima.m@gmail.com>
2025-01-15 16:25:25 -0300

    func: add entities verification to OperationService

--- src/main/java/tech/mlm/plutus/services/OperationService.java
+++ src/main/java/tech/mlm/plutus/services/OperationService.java
@@ -1,6 +1,7 @@
 package tech.mlm.plutus.services;
 
 import org.springframework.stereotype.Service;
+import tech.mlm.plutus.dtos.OperationDTO;
 import tech.mlm.plutus.entities.OperationEntity;
 import tech.mlm.plutus.entities.ProductEntity;
 import tech.mlm.plutus.entities.SellerEntity;
@@ -9,6 +10,7 @@ import tech.mlm.plutus.repositories.OperationRepository;
 import tech.mlm.plutus.types.OperationType;
 import tech.mlm.plutus.types.StatusType;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,9 +18,15 @@ import java.util.Optional;
 public class OperationService {
 
     private final OperationRepository operationRepository;
+    private final ProductService productService;
+    private final SellerService sellerService;
+    private final StoreService storeService;
 
-    public OperationService(OperationRepository operationRepository) {
+    public OperationService(OperationRepository operationRepository, ProductService productService, SellerService sellerService, StoreService storeService) {
         this.operationRepository = operationRepository;
+        this.productService = productService;
+        this.sellerService = sellerService;
+        this.storeService = storeService;
     }
 
     public Optional<OperationEntity> createOperation(OperationType operationType,
@@ -35,9 +43,71 @@ public class OperationService {
         return Optional.of(operationEntity);
     }
 
-    public Optional<OperationEntity> createOperation(OperationEntity operationEntity) {
+    public Optional<OperationEntity> save(OperationEntity operationEntity) {
         operationRepository.saveAndFlush(operationEntity);
         return Optional.of(operationEntity);
+    }
+
+    public OperationEntity createOperationEntity(OperationDTO dto) {
+        try {
+            validateEntities(dto);
+
+            return new OperationEntity(OperationType.valueOf(dto.operationType()),
+                                        productService.findByBarcode(dto.productBarcode()),
+                                        storeService.findById(dto.originStoreId()),
+                                        storeService.findById(dto.destinationStoreId()),
+                                        sellerService.findByCpf(dto.originSellerCpf()),
+                                        sellerService.findByCpf(dto.destinationSellerCpf()),
+                                        dto.quantity());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void validateEntities(OperationDTO dto){
+        validateOperationType(dto.operationType());
+        validateSeller(dto.originSellerCpf());
+        validateSeller(dto.destinationSellerCpf());
+        validateStore(dto.originStoreId());
+        validateStore(dto.destinationStoreId());
+        validateProduct(dto.productBarcode());
+        validateQuantity(dto.quantity());
+    }
+
+    private void validateSeller(String cpf){
+        try{
+            sellerService.findByCpf(cpf);
+
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void validateStore(Long id){
+        try {
+            storeService.findById(id);
+
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void validateProduct(String barcode){
+        try {
+            productService.findByBarcode(barcode);
+
+        } catch(Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void validateOperationType(String operationType){
+        if(Arrays.stream(OperationType.values())
+                 .noneMatch(type -> type.name().equals(operationType))) throw new IllegalArgumentException("Operation type not supported");
+    }
+
+    private void validateQuantity(int quantity){
+        if(quantity <= 0) throw new IllegalArgumentException("Quantity cannot be less or equal to zero");
     }
 
     public Optional<OperationEntity> setOperationType(Long operationId, OperationType operationType) {
