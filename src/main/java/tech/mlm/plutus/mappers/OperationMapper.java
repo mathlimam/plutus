84d866fd0f812ef46c00d61d683c50324d113190
Matheus <math.lima.m@gmail.com>
2025-01-20 15:03:19 -0300

    func: create toDTO and toEntity methods

--- src/main/java/tech/mlm/plutus/mappers/OperationMapper.java
+++ src/main/java/tech/mlm/plutus/mappers/OperationMapper.java
@@ -1,12 +1,43 @@
 package tech.mlm.plutus.mappers;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.NullValuePropertyMappingStrategy;
-import tech.mlm.plutus.dtos.OperationDTO;
+import org.springframework.stereotype.Component;
 import tech.mlm.plutus.entities.OperationEntity;
+import tech.mlm.plutus.dtos.OperationDTO;
+import tech.mlm.plutus.services.OperationService;
 
-@Mapper(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL)
-public interface OperationMapper {
-    OperationDTO toDTO(OperationEntity operation);
-    OperationEntity toEntity(OperationDTO dto);
+
+import java.util.Optional;
+
+@Component
+public class OperationMapper {
+    private final OperationService operationService;
+
+    public OperationMapper(OperationService operationService) {
+        this.operationService = operationService;
+    }
+
+    public Optional<OperationEntity> toEntity(OperationDTO dto){
+
+        try {
+            if (dto == null) throw new IllegalArgumentException("dto cannot be null");
+            return Optional.of(operationService.createOperationEntity(dto));
+
+        } catch (Exception e){
+            System.out.println(e.getLocalizedMessage());
+            return Optional.empty();
+        }
+    }
+
+    public OperationDTO toDto(OperationEntity entity){
+        return new OperationDTO(entity.getType().name(),
+                entity.getCreatedAt(),
+                entity.getUpdatedAt(),
+                entity.getStatus().name(),
+                entity.getOriginSeller().getId(),
+                entity.getDestinationSeller().getId(),
+                entity.getOriginStore().getId(),
+                entity.getDestinationStore().getId(),
+                entity.getProductEntity().getBarcode(),
+                entity.getQuantity());
+    }
 }
