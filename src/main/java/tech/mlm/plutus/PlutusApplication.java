64cc065546a0bdc352f73d0c316cbc2018d58aa7
Matheus <math.lima.m@gmail.com>
2025-02-22 23:39:06 -0300

    update main

--- src/main/java/tech/mlm/plutus/PlutusApplication.java
+++ src/main/java/tech/mlm/plutus/PlutusApplication.java
@@ -1,11 +1,12 @@
 package tech.mlm.plutus;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
 
 @SpringBootApplication
+@EntityScan(basePackages={"tech.mlm.plutus.entities","tech.lastbox.jwt"})
 public class PlutusApplication {
 
 	public static void main(String[] args) {
